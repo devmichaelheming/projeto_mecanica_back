433ca680c2de32ebe34ec0e8fd54de816ccfde7f
michael heming <devmichael.heming@gmail.com>
2023-03-23 17:44:52 -0400

    Subindo estrutura inicial do back

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://omnistack:fuMlBTSOg7nRpH5T@projeto-mecanica.gcfma9g.mongodb.net/test',
+    ),
+    UsersModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
