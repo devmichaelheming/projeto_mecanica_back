12ac99d169b89ac083a8d450ddaa4cda56661fff
michael heming <devmichael.heming@gmail.com>
2023-03-24 20:58:59 -0400

    fix: Adicionando campo de CPF

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,6 @@
 export class CreateUserDto {
   name: string;
   email: string;
+  cpf: string;
   password: string;
 }
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,5 +1,5 @@
-import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { HydratedDocument } from "mongoose";
 
 export type UserDocument = HydratedDocument<User>;
 
@@ -10,6 +10,9 @@ export class User {
 
   @Prop()
   email: string;
+
+  @Prop()
+  cpf: string;
 
   @Prop()
   password: string;
