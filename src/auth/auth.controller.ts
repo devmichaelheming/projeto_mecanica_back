0002dd9000b33c442a3e8594887c5dd67e91b7f1
michael heming <devmichael.heming@gmail.com>
2024-03-23 09:53:52 -0400

    fix: liberando acesso as rotas de signIn e signUp

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,16 +3,19 @@ import { AuthService } from "./auth.service";
 import { SignUpDto } from "./dto/signup.dto";
 import { SignInDto } from "./dto/signin.dto";
 import { AuthGuard } from "./auth.guard";
+import { Public } from "./metadataKeys";
 
 @Controller("auth")
 export class AuthController {
     constructor(private readonly authService: AuthService) { }
 
+    @Public()
     @Post('/signup')
     signUp(@Body() signUnDto: SignUpDto): Promise<{ access_token: string }> {
         return this.authService.signUp(signUnDto)
     }
 
+    @Public()
     @Get('/signin')
     signIn(@Body() signInDto: SignInDto): Promise<{ access_token: string }> {
         return this.authService.signIn(signInDto);
